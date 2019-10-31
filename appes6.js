cc9f27ff7326575a20008b6b0d607fdb44b876e3
Dariusz Okonski <darek200180@gmail.com>
2019-10-31 15:36:38 +0000

    add localStorage funcionality

--- appes6.js
+++ appes6.js
@@ -9,7 +9,6 @@ class Book {
 
 //UI Constructor
 class UI {
-
   addBookToList(book) {
     const list = document.getElementById('book-list');
 
@@ -17,7 +16,7 @@ class UI {
     row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
-      <td>${book.author}</td>
+      <td>${book.isbn}</td>
       <td><a href="#" class="delete">X</a></td>
     `
     list.appendChild(row);
@@ -51,6 +50,50 @@ class UI {
   }
 }
 
+//Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    const ui = new UI();
+    books.forEach(function (book) {
+      ui.addBookToList(book);
+    })
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+window.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 
 // Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
@@ -69,6 +112,9 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   } else {
     // add book to list
     ui.addBookToList(book);
+
+    Store.addBook(book);
+
     ui.showAlert('Book Added!', 'success');
 
     ui.clearFields();
@@ -83,6 +129,9 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
   const ui = new UI();
   ui.deleteBook(e.target);
+
+  // Remove from local storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   ui.showAlert('Book Removed', 'success');
 
